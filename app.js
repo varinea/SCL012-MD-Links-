488acac863e815251d8bc45e55b2976c0b8c7ccb
Varinea <varinea.lopez@gmail.com>
2020-02-14 11:33:38 -0300

    creando app.js

---
+++ app.js
@@ -0,0 +1,1 @@
+const mdlinks = require('./app')
--- index.js
+++ index.js
@@ -11,4 +11,16 @@ const markdown = (path)=>{
   return markdown;
   };
 
-  
+  //Leer los archivos
+  const readFile = (file=> {
+    let promise = new Promise ((resolve, reject) => {
+      fs.readFile(file, "UTF-8", (error, data) => {
+        if(error){
+          reject(new Error("Archivo no fue encontrado"))
+        }
+        resolve(data)
+      });
+    });
+    return promise
+  });
+  module.exports = readFile;
