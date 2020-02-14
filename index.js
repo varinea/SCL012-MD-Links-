e04b7959f7e768f28200dd71cd6207f2ed53d7cc
Varinea <varinea.lopez@gmail.com>
2020-02-14 11:04:03 -0300

    Revisa archivos MARKDOWN

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = () => {
-  // ...
-};
+const fs = require("fs");
+
+//Revisa si el archivo el MarkDown
+const markdown = (path)=>{
+  if(path.slice(-3) == ".md"){
+  console.log(path)
+  }
+  else{
+    console.log("error");
+  }
+  return markdown;
+  };
+
+  
